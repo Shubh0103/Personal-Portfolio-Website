3b446206a227ba5374a9216784cd3a2d07c69961
Shubh0103 <smpatil0103@gmail.com>
2022-04-20 11:42:21 +0530

    The project is completed till services section

---
+++ index.js
@@ -0,0 +1,46 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Portfolio section-Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
+//Our clients-swipper
+
+//Website dark/light theme
+
+//scroll to top button
+
+//Navigation menu items active on page scroll
+
+//Responsive navigation menu toggle
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+
+//Target elements, and specify options to creat reveal animations
+
+
